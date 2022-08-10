d89bfd33733a01426ec7e2e1f9fd4a1e86577ddc
MexiTi <100490397+MexiTi@users.noreply.github.com>
2022-08-10 18:59:39 +0200

    Simple Repair FiveM
    An Simple Reapir station script for FiveM

---
+++ CarRepairStation/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.RepairTime = 4000 --How long it takes to repair the vehicle
+Config.Blips = true -- Do you want blibs ?  No=false Yes=true
+
+Config.Stations = {
+	{x = -36.52, y = -1088.55, z = 26.02}, --XYZ Cordinates where you can repair your vehicles
+	{x = -211.15, y = -1323.98, z = 30.49},
+}
